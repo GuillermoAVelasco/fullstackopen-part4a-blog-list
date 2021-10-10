decc5d643ceb8f7b4dc9ee03eb729708e91d0c9a
Guillermo Velasco <alfadoble10@gmail.com>
2021-10-10 14:16:13 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+express = require('express')
+const config = require('./utils/config')
+const mongoose = require('mongoose')
+const notesRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const cors = require('cors')
+
+app = express()
+
+mongoose.connect(config.MONGOURL)
+
+app.use(cors())
+app.use(express.json())
+
+app.use(middleware.requestLogger)
+app.use('/api/blogs',notesRouter)  
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports=app
---
+++ utils/config.js
@@ -0,0 +1,4 @@
+require('dotenv').config()
+const PORT = process.env.PORT
+const MONGOURL = process.env.MONGOURL
+module.exports={PORT,MONGOURL}
