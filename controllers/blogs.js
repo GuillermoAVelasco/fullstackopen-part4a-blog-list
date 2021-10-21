ee64a0504680194366a505a95da753aff3eeb515
Guillermo Velasco <alfadoble10@gmail.com>
2021-10-21 15:34:25 -0300

    Ejercicio 4.6 Incorporando libreria Lodash. Ayuda bastante pero igual costo

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,3 +1,4 @@
+var _ = require('lodash');
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
@@ -20,5 +21,28 @@ blogsRouter.post('/', (request, response,next) => {
         })      
       .catch(e=>next(e))
 })
+
+const reducer= (sum)=>{ return sum+1}
+const mostBlogs = (blogs) => {
+  const xx=_.countBy(_.map(blogs,'author'), reducer)
+
+  const values=Object.values(xx)
+  const keys=Object.keys(xx)
+  
+  return keys.map((e,i)=> {
+    const cadena=`{"${e.substr(0,e.length-1)}":"${values[i]}"}`
+    return JSON.parse(cadena)    
+})
+}
+
+blogsRouter.get('/mostBlogs', (request, response,next) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(mostBlogs(blogs))
+      })
+    .catch(e=>next(e))
+})
+
 
 module.exports = blogsRouter
